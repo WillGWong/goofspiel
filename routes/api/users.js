58010a1b608d2ee8a77940f38338b08e0c8cbe15
Andy <github@sockbot.com>
2019-08-27 15:29:54 -0700

    change api/users route to show users obj

--- routes/api/users.js
+++ routes/api/users.js
@@ -9,28 +9,18 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = (db) => {
-  // router.get("/", (req, res) => {
-  //   db.query(`SELECT * FROM users;`)
-  //     .then(data => {
-  //       const users = data.rows;
-  //       res.json({ users });
-  //     })
-  //     .catch(err => {
-  //       res
-  //         .status(500)
-  //         .json({ error: err.message });
-  //     });
-  // });
-
-  // router.get("/", (req, res) => {
-  //   res.render("index")
-  // });
-
   router.get("/", (req, res) => {
-    console.log("hello")
-    res.render("users_index")
+    db.query(`SELECT * FROM users;`)
+      .then(data => {
+        const users = data.rows;
+        res.json({ users });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
   });
-
   return router;
 };
 

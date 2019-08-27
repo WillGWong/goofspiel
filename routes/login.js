f3546881a69ff6b8ca25a99fa17873157843899f
Andy <github@sockbot.com>
2019-08-27 16:59:39 -0700

    create new user and login existing user on POST to /login

--- routes/login.js
+++ routes/login.js
@@ -14,13 +14,50 @@ router.use(cookieSession({
   maxAge: 24 * 60 * 60 * 1000
 }));
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME
+});
+
+const loginUser = (user_email) => {
+  return pool.query(`
+  SELECT *
+  FROM users
+  WHERE email = $1;
+  `, [user_email])
+  .then(res => {
+    if (res.rows.length === 0) {
+      return pool.query(`
+      INSERT INTO users (email)
+      VALUES ($1)
+      RETURNING *;
+      `, [user_email])
+      .then(res => {
+        // console.log("Inserted and logged in new user:", res.rows[0]);
+        return res.rows[0];
+      });
+    } else {
+      // console.log("Logged in existing user:", res.rows[0])
+      return res.rows[0];
+    }
+  })
+}
+
 module.exports = (db) => {
   router.get("/", (req, res) => {
     res.render("login");
   });
 
   router.post("/", (req, res) => {
-    req.session.email  = req.body.email;
+    loginUser(req.body.email)
+    .then(res => {
+      console.log("!!!!!!!!!!!!!!!!!!!!!!!!!Logged in:", res.id)
+      req.session.user_id  = res.id;
+    });
+
     res.redirect("/")
   })
 

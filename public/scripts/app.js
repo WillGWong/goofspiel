b910a34d1111ba5eefb645f59d3a59893f1f2919
Willey Wong <willeyw@gmail.com>
2019-08-30 01:54:58 +0000

    added sound effects

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,42 +1,37 @@
-$(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
-});
-
-const guncock = "images/guncock.mp3"
 
 $(document).ready(function(){
   $("#1").click(function(){
-    $("input:text").val("1");
+    $('#guncock')[0].play();
+    $("input:text").val("1")
   });
 
   $("#2").click(function(){
+    $('#guncock')[0].play();
     $("input:text").val("2");
   });
 
   $("#3").click(function(){
+    $('#guncock')[0].play();
     $("input:text").val("3");
   });
 
   $("#4").click(function(){
+    $('#guncock')[0].play();
     $("input:text").val("4");
   });
 
   $("#5").click(function(){
+    $('#guncock')[0].play();
     $("input:text").val("5");
   });
 
   $("#6").click(function(){
+    $('#guncock')[0].play();
     $("input:text").val("6");
   });
 
   $("#7").click(function(){
+    $('#guncock')[0].play();
     $("input:text").val("7");
   });
 });
